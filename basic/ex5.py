7d4aec31bb6e6fdbe7170c56d1e51f1bd8655103
jpedraza <jpedrazam06@gmail.com>
2026-02-11 18:11:12 +0100

    S02

--- basic/ex5.py
+++ basic/ex5.py
@@ -4,4 +4,11 @@ def word_processer(lst):
     for ch in lst:
         print(ch, "->", len(ch), "characters")
 
+n = 1
+while n <= 1000:
+    print(n)
+    n = n * 2
+print(n)
+
 word_processer((words))
+
